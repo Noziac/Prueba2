45d12639cc20e23186dc556aca22fdc605cc979c
Noziac <jo.rivasa@duocuc.cl>
2025-04-16 14:21:08 -0400

    Creación modelos

--- Sumativa2/settings.py
+++ Sumativa2/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'Sumativa2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': '127.0.0.1:1521/orcl',
+        'USER': 'Sumativa2',
+        'PASSWORD': 'OracleCloud123',
+        'TEST': {
+            'USER': 'default_test',
+            'TBLSPACE': 'default_test_tbls',
+            'TBLSPACE_TMP': 'default_test_tbls_tmp'
+        }
     }
 }
 
--- core/models.py
+++ core/models.py
@@ -1,3 +1,73 @@
 from django.db import models
 
 # Create your models here.
+
+# Clase para los usuarios
+class Usuario(models.Model):       #Considerando usar AbastractUser
+    TIPOS_USUARIO = [
+        ('cliente', 'Cliente'),
+        ('administrador', 'Administrador'),
+    ]
+    id_usuario = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    contraseña = models.CharField(max_length=20)
+    tipo_usuario = models.CharField(max_length=15, choices=TIPOS_USUARIO, default='cliente')
+
+    def __str__(self):
+        return self.nombre
+
+# Clase para las versiones
+class Version(models.Model):
+    id_version = models.AutoField(primary_key=True)
+    nombre_version = models.CharField(max_length=50)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    caracteristicas = models.TextField()
+    activo = models.BooleanField(default=True) #Para quitar o poner versiones
+
+    def __str__(self):
+        return self.nombre_version
+    
+# Clase base para las extensiones (abstracta)    
+class BaseExtension(models.Model):
+    TIPOS_EXTENSION = [
+        ('inventario', 'Agrandar Inventario'),
+        ('skin', 'Skin'),
+        ('pve', 'PvE'),
+    ]
+    nombre_extension = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        abstract = True #No genera una tabla en la base de datos
+
+    def __str__(self):
+        return self.nombre_extension
+    
+# Subclase para extensiones de inventario
+class InvExtension(BaseExtension):
+    inv_extra = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.nombre_extension} (Inventario)"
+    
+# Subclase para skins BEAR
+class BearSkinExtension(BaseExtension):
+    skin = models.CharField(default= 'BEAR', max_length=25)
+
+    def __str__(self):
+        return f"{self.nombre_extension} (BEAR)"
+    
+# Subclase para skins USEC
+class UsecSkinExtension(BaseExtension):
+    skin = models.CharField(default= 'USEC', max_length=25)
+
+    def __str__(self):
+        return f"{self.nombre_extension} (USEC)"
+    
+# Subclase para el PVE
+class PveExtension(BaseExtension):
+    def __str__(self):
+        return f"{self.nombre_extension} (BEAR)"
